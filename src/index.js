10159c3b653a65af9b665d8d068dd0a7dcb9be21
Damon <tuidamon@gmail.com>
2020-10-02 23:15:16 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -5,82 +5,92 @@ const lineReader = require('line-reader'); // to read line one by one
 const fetch = require("node-fetch"); // to send request and get response
 // to match url with http and https
 const regex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g);
-//set the arguement
-const argv  = require("yargs") 
-    .scriptName("fbl")
-    .usage("Usage: $0 [options] <argument> where argument can be a file name or a url")
-    .example(
-        "$ fbl -f test.html or test.txt",
-        "process the file to find any broken link inside the file."
-    )
-    .example(
-        "$ fbl -u https://www.google.ca/",
-        "check the url if it is broken link or not."
-    )
-    .option("f", {
-        alias: "fileName",
-        describe: "Any file you want to check for broken link",
-        type: "array",
 
-    })
-    .option("u", {
-        alias: "url",
-        describe: "Any url you want to check for broken link",
-        type: "string",
+const opts = process.argv.slice(2);
 
-    })
-    .check((argv) => {
-        if(argv.f || argv.u){
-            handleArg(argv)
-            return true
-       }
-        console.log(chalk.red.bold('At least one arguement is required!'));
-        return false
-    })
-    .version('v', 'Show version number', '1.0.0')
-    .alias('v', 'version')
-    .help('h', "Show help.")
-    .alias('h', 'help')
-    .epilog("copyright 2020")
-    .argv;
+if (opts[0] == "/v" || opts[0] == "/version") {
+    console.log("Current version: 1.0.0");
+} else {
+
+    //set the arguement
+    const argv = require("yargs")
+        .scriptName("fbl")
+        .usage("Usage: $0 [options] <argument> where argument can be a file name or a url")
+        .example(
+            "$ fbl -f test.html or test.txt",
+            "process the file to find any broken link inside the file."
+        )
+        .example(
+            "$ fbl -u https://www.google.ca/",
+            "check the url if it is broken link or not."
+        )
+        .option("f", {
+            alias: "fileName",
+            describe: "Any file you want to check for broken link",
+            type: "array",
+
+        })
+        .option("u", {
+            alias: "url",
+            describe: "Any url you want to check for broken link",
+            type: "string",
+
+        })
+        .check((argv) => {
+            if (argv.f || argv.u) {
+                handleArg(argv)
+                return true
+            }
+            console.log(chalk.red.bold('At least one arguement is required!'));
+            return false
+        })
+        .version('v', 'Show version number', '1.0.0')
+        .alias('v', 'version')
+        .help('h', "Show help.")
+        .alias('h', 'help')
+        .epilog("copyright 2020")
+        .argv;
+}
 
 //handle when there is a arguement
-function handleArg(argv){
+function handleArg(argv) {
     //when arguement is a filename
-    if(argv.f){
+    if (argv.f) {
         readFile(argv.f)
-    //when arguemnt is a url
-    }else if(argv.u){
+        //when arguemnt is a url
+    } else if (argv.u) {
         checkUrlAndReport(argv.u)
     }
 }
 
 //send http request and check the status
-function checkUrlAndReport(url){
-    fetch(url, {method: "head", timeout: 13000})
-        .then(function (response){
-            if(response.status == 400 || response.status == 404){
+function checkUrlAndReport(url) {
+    fetch(url, { method: "head", timeout: 13000 })
+        .then(function (response) {
+            if (response.status == 400 || response.status == 404) {
                 console.log(chalk.red.bold("Bad ===> " + response.status + " ===> " + response.url));
-            }else if(response.status == 200){
+            } else if (response.status == 200) {
                 console.log(chalk.green.bold("Good ===> " + response.status + " ===> " + response.url));
-            }else{
-                console.log(chalk.grey.bold("Unknown ===> "+ response.status + " ===> "  + response.url));
+            } else if (response.status == 301 || response.status == 307 || response.status == 308) {
+                console.log(chalk.yellow.bold("Redirect ===> " + response.status + " ===> " + response.redirected));
+            } else {
+                console.log(chalk.grey.bold("Unknown ===> " + response.status + " ===> " + response.url));
             }
-        }).catch(function (err){
+        }).catch(function (err) {
             console.log(chalk.blue.bold("Not exist ===> 000 ===> " + url));
-        })    
+        })
 }
 
 // read each line of a file and call checkUrlandReport function
-function readFile(fileName){
+function readFile(fileName) {
     fileName.forEach(i => {
-        lineReader.eachLine(i, (line)=>{
+        lineReader.eachLine(i, (line) => {
             //find if any line conatins url with http and https
             let match_array = line.match(regex);
-            if(match_array != null){
+            if (match_array != null) {
                 match_array.forEach((i) => {
                     checkUrlAndReport(i);
-                });    
+                });
             }
         })
     });
